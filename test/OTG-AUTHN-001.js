9ba16e824e676bdd6e8c1706780d8fa4a1aa7015
Michele Porcarelli <porcarelli@halleymedia.com>
2019-11-28 11:56:48 +0100

    OTG-AUTHN-001 e OTG-CRYPST-003

---
+++ test/OTG-AUTHN-001.js
@@ -0,0 +1,19 @@
+'use strict';
+const Client = require("./services/client");
+describe("OTG-AUTHN-001",
+  () => {
+
+    it("returns a 400 status code when logging in over an unencrypted channel", async () => {
+      //Arrange
+      const client = new Client();
+      const credentials = {username:"username", password:"password"};
+
+      //Act
+      const { response } = await client.post(`http://${client.host}${client.basePath}/login`, credentials);
+
+      //Assert
+      expect(response.status).toEqual(400);
+      expect(response.data.violations[0].code).toBe("protocol-error");
+    }, 120000);
+  }
+);
---
+++ test/OTG-CRYPST-003.js
@@ -0,0 +1,19 @@
+'use strict';
+const Client = require("./services/client");
+describe("OTG-CRYPST-003",
+  () => {
+
+    it("returns a 400 status code when requesting data over an unencrypted channel", async () => {
+      //Arrange
+      const client = new Client();
+
+      //Act
+      await client.login();
+      const { response } = await client.get(`http://${client.host}${client.basePath}/videos`);
+
+      //Assert
+      expect(response.status).toEqual(400);
+      expect(response.data.violations[0].code).toBe("protocol-error");
+    }, 120000);
+  }
+);
